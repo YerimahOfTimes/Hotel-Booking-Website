2b88782ff580ab34c66b524192cb0aeb48f67284
YerimahOfTimes <yerimahjr@gmail.com>
2026-03-02 04:34:36 +0100

    Fix Express for Vercel

--- server/server.js
+++ server/server.js
@@ -2,23 +2,21 @@ import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import { clerkMiddleware} from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import clerkWebHooks from "./Controllers/clerkWebHooks.js";
 
 connectDB()
 
 const app = express()
-app.use(cors()) // enable cross-origin resources sharing
 
-// middleware
+app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-// API to listen to clerk webhooks
 app.use("/api/clerk", clerkWebHooks)
 
-app.get('/', (req,res)=> res.send('API is working'))
+app.get("/", (req, res) => res.send("API is working 🚀"))
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
+// ❌ REMOVE app.listen()
+// ✅ EXPORT INSTEAD
+export default app;
